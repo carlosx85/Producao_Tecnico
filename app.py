ebf98048f5d4cc1b153abd309264428dbe77c65b
carlosx85 <carlos.santosx85@hotmail.com>
2022-12-14 11:10:01 -0300

    t
    t

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ import plotly.graph_objects as go
 from PIL import Image
 import matplotlib
 import seaborn as sns
+from streamlit_lottie import st_lottie
 
-
-
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 
@@ -213,8 +217,29 @@ dx
 
 
 
-
-
+# ---- LOAD ASSETS ----
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
+ 
+# ---- WHAT I DO ----
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+        st.write(
+            """
+            On my YouTube channel I am creating tutorials for people who:
+            - are looking for a way to leverage the power of Python in their day-to-day work.
+            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
+            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
+            - are working with Excel and found themselves thinking - "there has to be a better way."
+            If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
+            """
+        )
+        st.write("<[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
+    with right_column:
+        st_lottie(lottie_coding, height=300, key="coding")
 
 
 
