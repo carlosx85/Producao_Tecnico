4c4411984a1ca40bdadc8a6765b330422b3d32eb
carlosx85 <carlos.santosx85@hotmail.com>
2022-12-14 13:10:57 -0300

    t
    t

--- app.py
+++ app.py
@@ -206,16 +206,11 @@ st.line_chart(chart_data)
 
 # LISTAGEM DIA
 st.title(":bar_chart: Listagem")
- 
-
 
 dx = pd.crosstab(df.Supervisor, df.Tipo , margins=True,  margins_name="Total")
-
-
 df
 
 st.title(":bar_chart: Producão Dia")
-
 dx
 
 
@@ -226,7 +221,7 @@ dx
 lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
  
 # ---- WHAT I DO ----
-with st.container():
+
     st.write("---")
     left_column, right_column = st.columns(2)
     with left_column:
