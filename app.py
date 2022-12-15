cce70c9cefe7bae6a2876f1947f41830388d2572
carlosx85 <carlos.santosx85@hotmail.com>
2022-12-15 16:08:41 -0300

    x
    x

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import matplotlib
 import seaborn as sns
 from streamlit_lottie import st_lottie
 import requests
-
+import yfinance as yf
 
 
 st.set_page_config(
@@ -210,17 +210,19 @@ st.line_chart(chart_data)
 
 
 
-# LISTAGEM DIA
-st.title(":bar_chart: Listagem")
+# LISTAGEM tipo
+st.title(":bar_chart: Listagem por Tipo")
 df
 
 
 dx = pd.crosstab(df.Supervisor, df.Tipo , margins=True,  margins_name="Total")
-st.title(":bar_chart: Produção Dia")
+st.title(":bar_chart: Produção Supervisor")
 dx
 
 
 
+# LISTAGEM DIA
+st.title(":bar_chart: Listagem por Dia")
 da=pd.crosstab(df.Tecnico, df.Dia , margins=True,  margins_name="Total")
 da
 
@@ -239,9 +241,32 @@ local_css("style.css")
 
 
 
+st.write("""
+# Simple Stock Price App
+Shown are the stock closing price and volume of Google!
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
+
+
+
+
+
+
+
 # ---- CONTACT ----
 with st.container():
-    st.write("<h3><B>Contato<h3></b>")
+    st.write("Contato")
     st.write("---")
  
 
