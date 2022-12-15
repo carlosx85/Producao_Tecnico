e8579ba9172618a933f5e34494a275ee97f0dd38
carlosx85 <carlos.santosx85@hotmail.com>
2022-12-15 16:26:57 -0300

    z
    z

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import matplotlib
 import seaborn as sns
 from streamlit_lottie import st_lottie
 import requests
-import yfinance as yf
+
 
 
 st.set_page_config(
@@ -234,29 +234,6 @@ def local_css(file_name):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 local_css("style.css")
 
-
-
-
-
-
-
-
-st.write("""
-# Simple Stock Price App
-Shown are the stock closing price and volume of Google!
-""")
-
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
-
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
 
 
 
